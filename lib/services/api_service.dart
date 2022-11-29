e86068ef9dd2a1d532e4e7547a7d35415f151bde
MatthiasMRC <matthias.marchione@gmail.com>
2022-11-29 21:30:49 +0100

    Ajout de la fonction getData et getPopularMovies

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 // Api class to call request
+import 'package:test_technique_flutter/models/movie_model.dart';
 import 'package:test_technique_flutter/services/api.dart';
 import 'package:dio/dio.dart';
+import 'package:test_technique_flutter/widgets/movie_card.dart';
 
 class ApiService {
   //Instance of Api class to access to the reference
@@ -9,22 +11,42 @@ class ApiService {
   //Instance of Dio package to call http request
   Dio DIO = Dio();
 
-  Future<Response> getHttp(String path, int page) async {
+  //Function to get the data with http request
+  Future<Response> getData(String path, int page) async {
     // url with dynamic "path" to select in the futur which type of movies we want to return eg: popular, etc...
     String _URL = API.BASE_URL + path;
 
-//Map with parameters that will always be called.
+    //Map with parameters that will always be called.
     Map<String, dynamic> _QUERY = {
       "api_key": API.API_KEY,
       "language": "fr-FR",
       "page": page
     };
 
-    //call the http request
     final response = await DIO.get(_URL, queryParameters: _QUERY);
 
     if (response.statusCode == 200) {
       return response;
+    } else {
+      throw response;
+    }
+  }
+
+  //Function to get Popular Movies
+  Future<List<MovieModel>> getPopularMovies() async {
+    Response response = await getData("/movie/popular", 1);
+
+    if (response.statusCode == 200) {
+      //Get all the data from results list
+      List<Map<String, dynamic>> results = response.data["results"];
+
+      //Add results to a Movie Model list
+      List<MovieModel> movies = [];
+      for (Map<String, dynamic> map in results) {
+        MovieModel movie = MovieModel.fromJson(map);
+        movies.add(movie);
+      }
+      return movies;
     } else {
       throw response;
     }
